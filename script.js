25b344e64f4d3296ff9751afb8ccebbeaf9176ab
Georgia Twumasi <twumasigm.vcu.edu>
2025-10-04 17:07:55 -0400

    commit changes

--- script.js
+++ script.js
@@ -33,3 +33,37 @@
     span.textContent = msg;
     msgEl.append(div,span);
 }
+
+// Check msg against the secret number
+function checkNumber(msg) {
+    const num = Number (msg); // msg = "hello world"
+
+    // Check if the spoken content is a valid number
+    if (Number.isNaN(num)) {
+        const div =document.createElement('div');
+        div.textContent = 'That is not a valid number'
+        msgEl.append(div);
+        return;
+    }
+
+    // Check if it's in range
+    if (num < 1 || num > 100) {
+        const div = document.createElement('div');
+        div.textContent = 'Number must be between 1 and 100';
+        msgEl.append(div);
+        return;
+    }
+
+    // Check the number and provide feedback
+    if (num === randomNum){
+        const h2 = document.createElement ('h2');
+        h2.textContent = 'Congrats! You have guessed the number! It was ${num}';
+
+        const button = document.createElement('button');
+        button.classList.add('play-again');
+        button.id = 'play-again' 
+        button.textContent = 'Play Again'
+
+        msgEl.append(h2, button);
+    }
+}
